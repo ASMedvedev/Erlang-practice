91f38e9dd43fea79faef8f64951dd9315ea42936
Aleksandr Medvedev <AMedvedev@gsa-soft.com.ua>
2015-06-06 01:26:36 +0300

    переделанные 2,3 задания

--- lecture 2/bs02.erl
+++ lecture 2/bs02.erl
@@ -1,4 +1,8 @@
 -module(bs02).
 -export([words/1]).
+-import(p05,[reverse/1]).
 
-words(BinText) -> binary:split(BinText, <<" ">>, [global]).
+words(Bin) -> words(Bin, <<>>, []).
+words(<<" ", Rest/binary>>, Word,Acc) -> words(Rest,<<>>,[Word|Acc]);
+words(<<X,Rest/binary>>, Word,Acc)->words(Rest,<<Word/binary,X>>,Acc);
+words(<<>>,Word,Acc)->reverse([Word|Acc]).
--- lecture 2/bs03.erl
+++ lecture 2/bs03.erl
@@ -1,4 +1,15 @@
 -module(bs03).
 -export([split/2]).
+-import(p05,[reverse/1]).
 
-split(BinText, S) -> binary:split(BinText, list_to_binary(S), [global]).
+split(Bin, S)-> split(Bin, list_to_binary(S), length(S), <<>>, []).
+
+split(Bin, S, Ln, Acc1, Acc2)->
+
+case Bin of
+	<<S:Ln/binary, Rest/binary>> -> 
+		split(<<Rest/binary>>, S, Ln, <<>>, [Acc1|Acc2]);
+	<<X, Rest/binary>> -> 
+		split(<<Rest/binary>>, S, Ln, <<Acc1/binary, X>>, Acc2);
+	<<>> -> reverse([Acc1|Acc2])
+end.
